d87cf711f8340fa6477876174c1ff5dd5dc12b6c
sami bekas <samibekas@gmail.com>
2019-03-05 12:10:13 +0100

    redux connected!

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,20 +1,28 @@
-import { Navigation } from 'react-native-navigation'
+import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "awesome-places.FindPlaceScreen",
-                label: "Find place",
-                title: "Find place"
-            },
-            {
-                screen: "awesome-places.SharePlaceScreen",
-                label: "Share place",
-                title: "Share place"
-            }
-        ]
-    })
-}
+    Promise.all([
+        Icon.getImageSource("md-map", 30),
+        Icon.getImageSource("ios-share-alt", 30)
+    ]).then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: "awesome-places.FindPlaceScreen",
+                    label: "Find Place",
+                    title: "Find Place",
+                    icon: sources[0]
+                },
+                {
+                    screen: "awesome-places.SharePlaceScreen",
+                    label: "Share Place",
+                    title: "Share Place",
+                    icon: sources[1]
+                }
+            ]
+        });
+    });
+};
 
-export default startTabs
+export default startTabs;
