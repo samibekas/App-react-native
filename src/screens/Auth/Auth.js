d5214ba80924536f2e2b1db03dfbf754b1ea7648
sami bekas <samibekas@gmail.com>
2019-03-06 12:49:27 +0100

    centered auth screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs'
 
 class AuthScreen extends Component {
@@ -10,12 +10,24 @@ class AuthScreen extends Component {
 
     render () {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Auth Screen</Text>
+                <Button title="Switch to login"/>
+                <TextInput placeholder="Your email address" />
+                <TextInput placeholder="Password" />
+                <TextInput placeholder="Confirm password" />
                 <Button title="Login" onPress={this.loginHandler}/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 export default AuthScreen;
